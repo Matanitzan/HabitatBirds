a57a504cf0e8a68c7750f0a5329e65e4d551bc77
jonatanBuga <yoni13445@gmail.com>
2023-05-19 19:27:10 +0300

    adding cage window and insert valus to Excel file

---
+++ HabitatBirdsApplication/HabitatBirdsApplication/Cage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitatBirdsApplication
+{
+    internal class Cage
+    {
+        private string serialNumber;
+        private float lenght, width, Heigth;
+        private string material;
+
+
+        public Cage(string serial,string material,float l,float w,float h)
+        {
+            setSerial(serial);
+            setMaterial(material);
+            setlenght(l);
+            setWidth(w);
+            setHeigth(h);
+        }
+        //set get for serial
+        public void setSerial(string serial)
+        {
+            this.serialNumber = serial;
+        }
+        public string getSerial() { return this.serialNumber; }
+
+
+        //set get for material
+        public void setMaterial(string material)
+        {
+            this.material = material;
+        }
+        public string getMaterial() { return this.material; }
+
+
+        //set get for lenght
+        public void setlenght(float l)
+        {
+            this.lenght = l;
+        }
+        public float getLenght() { return this.lenght; }
+
+
+        //set get for width
+        public void setWidth(float w)
+        {
+            this.width = w;
+        }
+        public float getWidth() { return this.width; }
+        
+
+        //set get for heigth
+        public void setHeigth(float h)
+        {
+            this.Heigth = h;
+        }
+        public float getHeigth() { return this.Heigth; }
+
+
+
+
+    }
+}
---
+++ HabitatBirdsApplication/HabitatBirdsApplication/NewCage.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using IronXL;
+namespace HabitatBirdsApplication
+{
+    /// <summary>
+    /// Interaction logic for NewCage.xaml
+    /// </summary>
+    public partial class NewCage : System.Windows.Window
+    {
+        List<Cage> cages ;
+        string path = @"C:\Users\Jonatan\Desktop\CageFile.xlsx";
+        public NewCage()
+        {
+            InitializeComponent();
+            cages = new List<Cage>();
+        }
+        private void openFile()
+        {
+            try
+            {
+                var excelApp = new Excel.Application();
+                excelApp.Visible = true;
+                Workbooks books = excelApp.Workbooks;
+                Workbook sheets = books.Open(path);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public static bool CheckString(string input)
+        {
+            // Regular expression pattern to match the required format
+            string pattern = @"^[A-Z][A-Za-z]*\d*$";
+
+            // Use Regex.IsMatch to check if the input matches the pattern
+            bool isMatch = Regex.IsMatch(input, pattern);
+
+            return isMatch;
+        }
+        
+        private void btnAddCage_Click(object sender, RoutedEventArgs e)
+        {
+
+            Cage cage = new Cage(SerialNumberText.Text, MetiralOptions.Text, float.Parse(LenghtCageText.Text, CultureInfo.InvariantCulture.NumberFormat),
+                float.Parse(WidthCageText.Text, CultureInfo.InvariantCulture.NumberFormat), float.Parse(HeightCageText.Text, CultureInfo.InvariantCulture.NumberFormat));
+            cages.Add(cage);
+
+
+            WorkBook myWorkBook = WorkBook.Load(path);
+
+            WorkSheet sheet = myWorkBook.GetWorkSheet("Sheet1");
+
+            string index = (cages.Count()+1).ToString();
+            sheet['A'+index].Value = cage.getSerial();
+            sheet['B'+index].Value = cage.getMaterial();
+            sheet['C'+index].Value = cage.getLenght();
+            sheet['D'+index].Value = cage.getWidth();
+            sheet['E'+index].Value = cage.getHeigth();
+
+            
+            myWorkBook.SaveAs(path);
+
+            //Excel.Application app = new Excel.Application();
+            //Workbook MyWorkBook = app.Workbooks.Add(System.Reflection.Missing.Value);
+            //Worksheet MySheets = (Worksheet)MyWorkBook.Worksheets.get_Item(1);
+
+
+            //MySheets.Cells[1, 1] = "Serial";
+            //MySheets.Cells[1, 1].Font.Bold = true;
+
+            //MySheets.Cells[1, 2] = "Metiral";
+            //MySheets.Cells[1, 2].Font.Bold = true;
+
+            //MySheets.Cells[1, 3] = "Length";
+            //MySheets.Cells[1, 3].Font.Bold = true;
+
+            //MySheets.Cells[1, 4] = "Witdh";
+            //MySheets.Cells[1, 4].Font.Bold = true;
+
+            //MySheets.Cells[1, 5] = "Height";
+            //MySheets.Cells[1, 5].Font.Bold = true;
+
+            //int i = 2;
+            //foreach(Cage cage1 in cages)
+            //{
+            //    MySheets.Cells[i, 1] = cage1.getSerial();
+            //    MySheets.Cells[i, 2] = cage1.getMaterial();
+            //    MySheets.Cells[i, 3] = cage1.getLenght();
+            //    MySheets.Cells[i, 4] = cage1.getWidth();
+            //    MySheets.Cells[i, 5] = cage1.getHeigth();
+            //    i++;
+            //}
+
+
+
+            //MyWorkBook.SaveAs2(path, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, XlSaveAsAccessMode.xlExclusive,
+            //    System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value);
+            //MessageBoxResult result = MessageBox.Show("do you want to open the file?", "opennig file", MessageBoxButton.YesNo, MessageBoxImage.Information);
+
+            //if (result.Equals(MessageBoxResult.Yes))
+            //{
+            //    openFile();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("your file located in file folder", "located file", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            //}
+            //MyWorkBook.Close(System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value);
+            //app.Quit();
+
+            //GC.WaitForPendingFinalizers();
+            //GC.Collect();
+            //GC.WaitForPendingFinalizers();
+            //GC.Collect();
+
+            //Marshal.ReleaseComObject(MySheets);
+            //Marshal.ReleaseComObject(MyWorkBook);
+            //Marshal.ReleaseComObject(app);
+
+            //this.Close();
+            Trace.WriteLine("serial:" + cage.getSerial());
+
+        }
+    }
+}
